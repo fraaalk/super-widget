d203048fc2a8fe64f3aa9873b7b0c90c6b53aee4
Frank Talk <frank.talk@kinoheld.de>
2017-01-21 21:21:43 +0100

    scss cleanup to remove redundancy in carousel

--- src/components/carousel/carousel.vue
+++ src/components/carousel/carousel.vue
@@ -284,176 +284,92 @@ export default {
 @import "../../vars";
 @import "../../../node_modules/breakpoint-sass/stylesheets/breakpoint";
 
+$carouselVariations: (
+  2: 50%,
+  3: 33.33333333333333%,
+  4: 25%,
+  5: 20%,
+  6: 16.6666666667%,
+  7: 14.28571428571429%
+);
+
 // http://madewithenvy.com/ecosystem/articles/2015/exploring-order-flexbox-carousel/
 .carousel {
-    display: flex;
+  display: flex;
 
     &__wrapper {
-        width: 100%;
-        overflow: hidden;
-        margin: 0 1px;
+      width: 100%;
+      overflow: hidden;
+      margin: 0 1px;
     }
 
     &__stage {
-        display: flex;
-        flex-grow: 1;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        position: relative;
-        left: -100%;
-        transform: translateX(100%);
+      display: flex;
+      flex-grow: 1;
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      position: relative;
+      left: -100%;
+      transform: translateX(100%);
 
-        .is-inactive & {
-            transform: 0 !important;
-            left: 0 !important;
-        }
+      .is-inactive & {
+        transform: 0 !important;
+        left: 0 !important;
+      }
 
-        &.is-reversing {
-            transform: translateX(-100%);
-        }
+      &.is-reversing {
+        transform: translateX(-100%);
+      }
 
-        &.is-set {
+      &.is-set {
+        transform: none;
+        transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1);
+      }
+
+      @each $variationName, $value in $carouselVariations {
+        .carousel--#{$variationName} & {
+          left: -$value;
+          transform: translateX($value);
+
+          &.is-reversing {
+            transform: translateX(-$value);
+          }
+
+          &.is-set {
             transform: none;
             transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1);
+          }
         }
-
-        // 2
-        .carousel--2 & {
-            left: -50%;
-            transform: translateX(50%);
-
-            &.is-reversing {
-                transform: translateX(-50%);
-            }
-
-            &.is-set {
-                transform: none;
-                transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1);
-            }
-        }
-
-        // 3
-        .carousel--3 & {
-            left: -33.33333333333333%;
-            transform: translateX(33.33333333333333%);
-
-            &.is-reversing {
-                transform: translateX(-33.33333333333333%);
-            }
-
-            &.is-set {
-                transform: none;
-                transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1);
-            }
-        }
-
-        // 4
-        .carousel--4 & {
-            left: -25%;
-            transform: translateX(25%);
-
-            &.is-reversing {
-                transform: translateX(-25%);
-            }
-
-            &.is-set {
-                transform: none;
-                transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1);
-            }
-        }
-
-        // 5
-        .carousel--5 & {
-            left: -20%;
-            transform: translateX(20%);
-
-            &.is-reversing {
-                transform: translateX(-20%);
-            }
-
-            &.is-set {
-                transform: none;
-                transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1);
-            }
-        }
-
-        // 6
-        .carousel--6 & {
-            left: -16.6666666667%;
-            transform: translateX(16.6666666667%);
-
-            &.is-reversing {
-                transform: translateX(-16.6666666667%);
-            }
-
-            &.is-set {
-                transform: none;
-                transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1);
-            }
-        }
-
-        // 7
-        .carousel--7 & {
-            left: -14.28571428571429%;
-            transform: translateX(14.28571428571429%);
-
-            &.is-reversing {
-                transform: translateX(-14.28571428571429%);
-            }
-
-            &.is-set {
-                transform: none;
-                transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1);
-            }
-        }
+      }
     }
 
     &__slide {
-        flex: 1 0 100%;
-        order: 2;
-        margin-right: 1px;
-        min-width: 0; // hack to make text ellipsis work inside of flexbox
+      flex: 1 0 100%;
+      order: 2;
+      margin-right: 1px;
+      min-width: 0; // hack to make text ellipsis work inside of flexbox
 
-        &.is-ref {
-            order: 1;
-            margin-right: 0;
+      &.is-ref {
+        order: 1;
+        margin-right: 0;
+      }
+
+      // If the carousel is inactive equal all slides
+      // and remove the ordering for the referenced one
+      .is-inactive & {
+        order: 0 !important;
+
+        &:last-child {
+          margin-right: 0;
         }
+      }
 
-        // If the carousel is inactive equal all slides
-        // and remove the ordering for the referenced one
-        .is-inactive & {
-            order: 0 !important;
-
-            &:last-child {
-                margin-right: 0;
-            }
+      @each $variationName, $value in $carouselVariations {
+        .carousel--#{$variationName} & {
+          flex-basis: $value;
         }
-
-        .carousel--2 & {
-            flex-basis: 50%;
-        }
-
-        // default
-        .carousel--3 & {
-            flex-basis: 33.33333333333333%;
-        }
-
-        .carousel--4 & {
-            flex-basis: 25%;
-        }
-
-        .carousel--5 & {
-            flex-basis: 20%;
-        }
-
-        .carousel--6 & {
-            flex-basis: 16.6666666667%;
-        }
-
-        .carousel--7 & {
-            flex-basis: 14.28571428571429%;
-        }
+      }
     }
 }
 </style>
